cfe149082a9ebb5b1551eb2108f8a3d5f11a701b
mkawsar <kawsar.diu888@gmail.com>
2020-06-29 13:43:10 +0600

    Notes curd completed

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
 	const notes = require('../controllers/note.controller');
 
 	app.post('/notes/create', notes.create);
+	app.get('/notes/list', notes.getAll);
+	app.get('/notes/:noteId/details', notes.details);
+	app.put('/notes/:noteId/update', notes.update);
+	app.delete('/notes/:noteId/delete', notes.delete);
 }
